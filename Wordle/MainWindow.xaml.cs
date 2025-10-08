ba15755ca6dd9079b69b57f0bff089d0709daaa1
Vynx777 <bartekfiksa@gmail.com>
2025-10-08 09:32:42 +0200

    Update

--- Wordle/MainWindow.xaml.cs
+++ Wordle/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,11 +21,15 @@ namespace Wordle
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private int stage = 0;
+        private string answerText = "pedał";
+        private ContentControl[,] charBlocks;
         public MainWindow()
         {
             InitializeComponent();
 
-            string tekst = "banan";
+            charBlocks = new ContentControl[5,6];
 
             for (int x = 0; x < 5; x++)
             {
@@ -33,11 +38,13 @@ namespace Wordle
                     ContentControl contentControl = new ContentControl
                     {
                         Template = (ControlTemplate)FindResource("CharBlockTemplate"),
-                        Content = tekst[x],
                         HorizontalAlignment = HorizontalAlignment.Stretch,
                         VerticalAlignment = VerticalAlignment.Stretch,
                         Margin = new Thickness(15, 5, 15, 5)
                     };
+
+                    charBlocks[x, y] = contentControl;
+
                     Grid.SetRow(contentControl, y);
                     Grid.SetColumn(contentControl, x);
                     mainGrid.Children.Add(contentControl);
@@ -45,9 +52,85 @@ namespace Wordle
             }
         }
 
+        private void Reset()
+        {
+            stage = 0;
+            textInput.Text = "";
+            for (int x = 0; x < 5; x++)
+            {
+                for (int y = 0; y < 6; y++)
+                {
+                    charBlocks[x, y].Content = "";
+                    charBlocks[x, y].Background = Brushes.White;
+                }
+            }
+        }
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            string tekst = textInput.Text;
 
+            if (tekst.Length != 5 || Regex.IsMatch(tekst, @"\d"))
+            {
+                MessageBox.Show($"Błąd! {tekst} nie jest prawidłowym słowem");
+                return;
+            }
+
+            if (stage < 0 || stage > 5)
+            {
+                MessageBox.Show("Koniec gry!");
+                return;
+            }
+
+            bool[] green = new bool[5];
+            int green_count = 0;
+            for (int x = 0; x < 5; x++)
+            {
+                charBlocks[x, stage].Content = tekst[x].ToString();
+
+                if (tekst[x] == answerText[x])
+                {
+                    green[x] = true;
+                    green_count++;
+                    charBlocks[x, stage].Background = Brushes.Green;
+                }
+            }
+
+            int[] counts = new int[26];
+            for (int i = 0; i < 5; i++)
+            {
+                if (!green[i])
+                {
+                    char c = char.ToLowerInvariant(answerText[i]);
+                    if (c >= 'a' && c <= 'z')
+                        counts[c - 'a']++;
+                }
+            }
+
+            for (int x = 0; x < 5; x++)
+            {
+                if (green[x]) continue;
+
+                char g = char.ToLowerInvariant(tekst[x]);
+                if (g >= 'a' && g <= 'z' && counts[g - 'a'] > 0)
+                {
+                    charBlocks[x, stage].Background = Brushes.Yellow;
+                    counts[g - 'a']--;
+                }
+                else
+                {
+                    charBlocks[x, stage].Background = Brushes.Gray;
+                }
+            }
+
+            if (stage >= 5 || green_count == 5)
+            {
+                MessageBox.Show("Koniec gry!");
+                Reset();
+                return;
+            }
+
+            stage++;
         }
     }
 }
